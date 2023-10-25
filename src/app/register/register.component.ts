d94fd7f2d959fad705d6b8cdc815ed247093859a
AfraTetva <115149533+AfraTetva@users.noreply.github.com>
2023-10-25 16:35:03 +0700

    angular_212102382

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, Renderer2} from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, "register-page");
+  }
+  ngOnInit(): void{
+
+  }
+}
